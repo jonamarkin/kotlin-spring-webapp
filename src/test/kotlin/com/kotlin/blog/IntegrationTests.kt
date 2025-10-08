983d23973b9482cc7250b0c7b0cbf195536e0fac
Jonathan Ato Markin <jona10711@gmail.com>
2025-10-08 19:02:06 +0200

    Integration tsts

---
+++ src/test/kotlin/com/kotlin/blog/IntegrationTests.kt
@@ -0,0 +1,20 @@
+package com.kotlin.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+
+    @Test
+    fun `Assert blog page title, content and status code`() {
+        val entity = restTemplate.getForEntity<String>("/")
+        assert(entity.statusCode.is2xxSuccessful)
+        assert(entity.body!!.contains("<title>Kotlin Blog</title>"))
+        assert(entity.body!!.contains("<h1>Kotlin Blog</h1>"))
+    }
+}
